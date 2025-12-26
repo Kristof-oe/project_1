30fe3f7e8ed72a773c2c2842216c97041203b995
Kristof-ay <kris200036@gmailcom>
2025-12-26 17:08:19 +0100

    Making test for task processing

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,3 +6,10 @@ m = TestClient(app)
 def test_health():
     response = m.get("/health")
     assert response.status_code == 200
+
+
+def test_process():
+    response = m.post("/task/")
+    assert response["status"] == "done"
+
+
